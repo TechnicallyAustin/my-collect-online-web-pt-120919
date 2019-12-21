b4a9adcef4e95f88b07ecb5885b2468d13af30bd
Austin Johnson <technicallyaustin@gmail.com>
2019-12-21 21:42:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,13 +5,6 @@ def my_collect(array)
   while i < array.length do
     yield array[i]
     array[i] = array[i].split(" ").first 
-    
-    if array[i] == array[i].upcase do
-      new_arr << array[i].downcase
-      
-      else array[i] == array[i].downcase do
-        new_arr << array[i].upcase
-end
     i += 1
   end
   new_arr

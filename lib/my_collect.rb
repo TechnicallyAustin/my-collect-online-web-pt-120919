e2f5caae080644d3efdfc0a8ed9876f78a0b7ca9
Austin Johnson <technicallyaustin@gmail.com>
2019-12-21 22:02:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(array)
   i = 0 
   new_arr = []
   while i < array.length do
-     new_arr << array[i]
-    yield(new_arr[i])
+     yield << array[i]
+    
     i += 1
   end
   new_arr

eb88d0e0a88989d166aa458bc2ed84952eb73d94
Austin Johnson <technicallyaustin@gmail.com>
2019-12-21 21:27:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,13 +3,14 @@ def my_collect(array)
   i = 0 
   new_arr = []
   while i < array.length do
-    yield array[i]
+    yield(array[i])
     i += 1
   end
   new_arr
 end
 
 my_collect(array) do |capital|
-  new_arr <<
+  new_arr << capital.split(" ").first
+end
 
     

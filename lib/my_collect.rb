c3986a5c4b15507f9470500335718f10f01840c6
Austin Johnson <technicallyaustin@gmail.com>
2019-12-21 21:19:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,14 @@
-def my_collect(array)
-  counter = 0 
+
+def my_collection(array)
+  i = 0 
   new_arr = []
-  while array.count > counter do 
-   # array { |seperate| seperate.split(" ").first }
-   if array[counter].include?(" ") do |seperate|
-     new_arr << seperate.split(" ").first }
-   else
-    i = array[counter]
-    yield i
-    new_arr << i.upcase
-    counter += 
+  while i < array.length do
+    yield array[i]
+    i += 1
   end
-  
-  end
-  
-end
-p new_arr
+  new_arr
 end
 
+my_collection(array) { new_arr << |name| name.split(" ".first)}
+
+    
